540809ea63d368a3d0bcf2718908146965cc263e
Fabian Ferno <57835412+fabianferno@users.noreply.github.com>
2020-10-06 10:09:55 +0530

    Write multiply.c

--- Multiply.c
+++ Multiply.c
@@ -1,1 +1,17 @@
-
+#include<stdio.h>
+void main()
+{
+    int num1, num2, product;
+    printf("\n=== PRODUCT CALCULATOR ===\n");
+  
+    // Getting the input
+    printf("\nEnter the First Number: ");
+    scanf("%d",&num1);
+    printf("\nEnter the Second Number: ");
+    scanf("%d",&num2);
+  
+    // Calculating the product
+    product = num1 * num2;
+  
+    printf("\nThe Product of those numbers is %d", product);
+}
