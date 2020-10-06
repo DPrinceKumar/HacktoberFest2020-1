43e3c5c67338fe97f204e78c5bda48a8cc6e9c6b
Samyak Jain <57041073+m-rNOBODY@users.noreply.github.com>
2020-10-06 21:31:08 +0530

    Update Adding-two-number.c
    Added code for adding two integers in c language.

--- Adding-two-number.c
+++ Adding-two-number.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+int main() {    
 
+    int number1, number2, sum;
+    
+    printf("Enter two integers: ");
+    scanf("%d %d", &number1, &number2);
+    sum = number1 + number2;      
+    
+    printf("%d + %d = %d", number1, number2, sum);
+    return 0;
+}
