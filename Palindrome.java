2c65e36cc30f1daf73e88b97180f2e35872a1308
Aditya060 <51037240+Aditya060@users.noreply.github.com>
2020-10-11 21:40:28 +0530

    Update Palindrome.java
    Added comments

--- Palindrome.java
+++ Palindrome.java
@@ -3,18 +3,18 @@ class Palindrome
 {
     public static void main(String s[])
     {
-    Scanner k=new Scanner(System.in);
+    Scanner k=new Scanner(System.in);//Create a scanner object
     System.out.println("Enter the first String");
-    String s1=k.nextLine();
+    String s1=k.nextLine();//Accept string
     String palindrome="";;
     for(int i=0;i<s1.length();i++)
     {
-        palindrome=s1.charAt(i)+palindrome;
+        palindrome=s1.charAt(i)+palindrome;//Reverses String
     }
     if(palindrome.equals(s1))
-    System.out.println("The reversed string is is "+ palindrome +" and it is a palindrome");
+    System.out.println("The reversed string is is "+ palindrome +" and it is a palindrome");//Displays String
     else
-    System.out.println("The reversed string is is"+ palindrome +"and it is not a palindrome");
+    System.out.println("The reversed string is is"+ palindrome +"and it is not a palindrome");//Displays String
     
 }
 }
