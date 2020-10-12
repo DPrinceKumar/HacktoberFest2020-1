4b0cbdf86c6dec9caee989d107896171e802571d
Amit Kumar <59211195+itsaamyth@users.noreply.github.com>
2020-10-13 00:07:36 +0530

    Update Subtract.c

--- Subtract.c
+++ Subtract.c
@@ -1,1 +1,20 @@
+#include<stdio.h>
 
+int main()
+{
+	int a,b,sub;
+
+	//Read value of a
+	printf("Enter the first no.: ");
+	scanf("%d",&a);
+
+	//Read value of b
+	printf("Enter the second no.: ");
+	scanf("%d",&b);
+
+	//formula of subtraction
+	sub= a-b;
+	printf("subtract is = %d\n", sub);
+
+	return 0;
+}
