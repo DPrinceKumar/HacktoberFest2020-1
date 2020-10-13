0ef09f0e42168607e1e5179073757c8e81c20ac0
pal-gupta97 <72738074+pal-gupta97@users.noreply.github.com>
2020-10-13 10:53:40 +0530

    Update subtract.c

--- Subtract.c
+++ Subtract.c
@@ -1,1 +1,17 @@
-
+#include<stdio.h>
+int main()
+{
+  int a,b,c;
+  printf("Enter Two numbers: ");
+  scanf("%d %d",&a,&b);
+  if(a>b)
+  {
+    c=a-b;
+  }
+  else if(b>a)
+  {
+    c=b-a;
+  }
+  printf("Subtraction of given two numbers is %d",c);
+  retrn 0;
+}
