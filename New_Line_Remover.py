56c5e387ab5a228315abf315724edebd59d06811
Sandesh Thapa Magar <66560689+Tsandesh@users.noreply.github.com>
2020-10-14 11:18:45 +0545

    Delete New_Line_Remover.py

--- New_Line_Remover.py
+++
@@ -1,9 +0,0 @@
-#Thank you for this repo DPrinceKumar
-#THIS WILL REMOVE EXTRA SPACES FROM YOUR TEXT
-
-Final = ''
-text = input('TEXT HERE :\n')
-for index, char in enumerate(text):
-        if not(text[index] == " " and text[index+1]==" "):
-            Final = Final + char
-print(Final)
