28a5895a4a559e10159ef3a289c63f11a8764ec0
qausainmanzoor piaic q2 <qm.reborn@yahoo.com>
2020-10-18 00:39:44 +0500

    culculator added by Qausain Manzoor

---
+++ Python/calculator.py
@@ -0,0 +1,21 @@
+#1st Assignment by Qausain Manzoor
+
+print("Addition")
+a = int(input("Enter 1st number "))
+b = int(input("Enter 2nd number "))
+print(a + b)
+
+print("Multiplication")
+a = int(input("Enter 1st number "))
+b = int(input("Enter 2nd number "))
+print(a * b)
+
+print("Subtraction")
+a = int(input("Enter 1st number "))
+b = int(input("Enter 2nd number "))
+print(a - b)
+
+print("Division")
+a = int(input("Enter 1st number "))
+b = int(input("Enter 2nd number "))
+print(a / b)
