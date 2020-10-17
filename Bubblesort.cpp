0a616f72d9b004177dd78f0d201845f0e36db20c
ms-svg <72918742+ms-svg@users.noreply.github.com>
2020-10-17 18:36:26 +0530

    Update Bubblesort.cpp

--- Bubblesort.cpp
+++ Bubblesort.cpp
@@ -39,3 +39,31 @@ int main()
     printArray(arr, n);  
     return 0;  
 }  
+
+//Driver code by manpreet.
+from sys import stdin
+def bubbleSort(arr, n) :
+    for i in range(n - 1) :
+        for j in range(n - i - 1) :
+            if arr[j] > arr[j+1] :
+                temp = arr[j] 
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp 
+def takeInput() : 
+    n = int(input().strip()) 
+    if n == 0 : 
+        return list(), 0
+    arr = list(map(int, input().strip().split(" "))) 
+    return arr, n
+def printList(arr, n) :
+    for i in range(n) :
+        print(arr[i], end = " ") 
+    print() 
+t = int(input().strip()) 
+while t > 0 :
+    arr, n = takeInput()
+    bubbleSort(arr, n)
+    printList(arr, n) 
+    t-= 1
+
+
