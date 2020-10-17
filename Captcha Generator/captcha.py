6f2de4675ad7f25032e07d382799e0b432a73418
Amey Band <ameypband23@gmail.com>
2020-10-17 12:51:21 +0530

    Create captcha.py

---
+++ Captcha Generator/captcha.py
@@ -0,0 +1,12 @@
+#Code By Amey Band
+
+from captcha.audio import AudioCaptcha
+from captcha.image import ImageCaptcha
+
+image = ImageCaptcha()
+data = image.generate('12AFGcvd')
+image.write('12AFGcvd', 'op.png')
+
+audio = AudioCaptcha()
+data = audio.generate('59647834')
+audio.write('59647834', 'op.wav')
