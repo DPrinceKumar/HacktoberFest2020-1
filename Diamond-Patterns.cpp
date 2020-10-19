f892d3bf6abd4f1494e6b94dfb28a4a3ebfa3996
Aatif Sarfaraz <42997579+aatif547@users.noreply.github.com>
2020-10-19 12:49:12 +0530

    diamond patter using java

--- Diamond-Patterns.cpp
+++ Diamond-Patterns.cpp
@@ -25,19 +25,17 @@ void printDiamond(int n)
 		space--; 
 	} 
 
-	// Repeat again in reverse order 
+
 	space = 0; 
 
-	// run loop (parent loop) 
-	// till number of rows 
+
 	for (int i = n; i > 0; i--) 
 	{ 
-		// loop for initially space, 
-		// before star printing 
+		
 		for (int j = 0; j < space; j++) 
 			cout << " "; 
 
-		// Print i stars 
+		
 		for (int j = 0;j < i;j++) 
 			cout << "* "; 
 
@@ -46,9 +44,59 @@ void printDiamond(int n)
 	} 
 } 
 
-// Driver code 
 int main() 
 { 
 	printDiamond(5); 
 	return 0; 
 }
+..//java
+import java.util.*; 
+
+class diamond
+{ 
+	
+	static void printDiamond(int n) 
+	{ 
+		int space = n - 1; 
+	
+		for (int i = 0; i < n; i++) 
+		{ 
+			
+			for (int j = 0; j < space; j++) 
+				System.out.print(" "); 
+	
+			for (int j = 0; j <= i; j++) 
+				System.out.print("* "); 
+	
+			System.out.print("\n"); 
+			space--; 
+		} 
+	
+	
+		space = 0; 
+	
+		
+		for (int i = n; i > 0; i--) 
+		{ 
+			// loop for initially space, 
+			// before star printing 
+			for (int j = 0; j < space; j++) 
+				System.out.print(" "); 
+	
+			// Print i stars 
+			for (int j = 0; j < i; j++) 
+				System.out.print("* "); 
+	
+			System.out.print("\n"); 
+			space++; 
+		} 
+	} 
+	
+	public static void main(String[] args) 
+	{ 
+		printDiamond(5); 
+		
+	} 
+} 
+
+
