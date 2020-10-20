c974b0446fa167035bda14cccef74a9389120ad9
ManjushaTVS <69451319+ManjushaTVS@users.noreply.github.com>
2020-10-20 21:47:18 +0530

    update_rockpaperscissorsshoot.py

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,34 +1,27 @@
-""" Rock Paper Scissors
-----------------------------------------
-"""
 import random
-import os
 import re
-os.system('cls' if os.name=='nt' else 'clear')
-while (1 < 2):
-    print "\n"
-    print "Rock, Paper, Scissors - Shoot!"
-    userChoice = raw_input("Choose your weapon [R]ock], [P]aper, or [S]cissors: ")
-    if not re.match("[SsRrPp]", userChoice):
-        print "Please choose a letter:"
-        print "[R]ock, [S]cissors or [P]aper."
-        continue
-    // Echo the user's choice
-    print "You chose: " + userChoice
-    choices = ['R', 'P', 'S']
-    opponenetChoice = random.choice(choices)
-    print "I chose: " + opponenetChoice
-    if opponenetChoice == str.upper(userChoice):
-        print "Tie! "
-    #if opponenetChoice == str("R") and str.upper(userChoice) == "P"
-    elif opponenetChoice == 'R' and userChoice.upper() == 'S':      
-        print "Scissors beats rock, I win! "
-        continue
-    elif opponenetChoice == 'S' and userChoice.upper() == 'P':      
-        print "Scissors beats paper! I win! "
-        continue
-    elif opponenetChoice == 'P' and userChoice.upper() == 'R':      
-        print "Paper beat rock, I win! "
-        continue
-    else:       
-        print "You win!"
+print("Rock,Paper,Scissor, shoot!")
+userchoice = input("choose your weapon [R]ock, [P]aper, [S]cissor :")
+if not re.match("[SsRrPp]", userchoice):
+    print ("Please choose a letter:")
+    print ("[R]ock, [S]cissors or [P]aper.")
+print("You chose:",userchoice)
+choices=['S','R','P']
+opponentchoice = random.choice(choices)
+print("I chose:",opponentchoice)
+if opponentchoice == userchoice:
+    print("Tie!!!")
+elif opponentchoice == 'R' and userchoice == 'S':
+    print("Rock beats Scissors, YOU LOST :(")
+elif opponentchoice == 'P' and userchoice == 'R':
+    print("Paper beats Rock, YOU LOST :(")
+elif opponentchoice == 'S' and userchoice == 'P':
+    print("Sciccors beats Paper, YOY LOST :(")
+elif opponentchoice == 'S' and userchoice == 'R':
+    print("Rock beats Scissors, YOU WIN :)")
+elif opponentchoice == 'R' and userchoice == 'P':
+    print("Paper beats Rock, YOU WIN :)")
+elif opponentchoice == 'P' and userchoice == 'S':
+    print("Sciccors beats Paper, YOY WIN :)")
+else:
+    print("Enter valid weapon!")
