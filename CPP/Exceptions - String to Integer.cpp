99bf0d13842ed0aa4817ad0e8dc14c61626e6ab5
Mahak Agarwal <65973339+missmahi55@users.noreply.github.com>
2020-10-20 20:43:18 +0530

    Create Exceptions - String to Integer.cpp
    https://www.hackerrank.com/challenges/30-exceptions-string-to-integer/problem

---
+++ CPP/Exceptions - String to Integer.cpp
@@ -0,0 +1,42 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    string S;
+    cin >> S;
+    try
+    {
+        int num = stoi(S);
+        cout<<num<<endl;
+    }
+    catch(exception)
+    {
+        cout<<"Bad String"<<endl;
+    }
+
+
+    return 0;
+}
