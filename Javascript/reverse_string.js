a1d94193779c431a79ba5455d5473828cc484602
Babatunde Abdulhameed Olakunle <humiditii45@gmail.com>
2020-10-21 07:33:42 +0000

    reverse string by sebago

--- Javascript/reverse_string.js
+++ Javascript/reverse_string.js
@@ -1,37 +1,75 @@
-// Reverse A string.
+//The problem way
+let x = '32243';
+const y = [...x];
+let reverseArray = [];
+for (let index = 0; index <= y.length; index++) {
+   
+   const element = y[y.length-index];
+   reverseArray.push(element);
+    
+}
+//const leng = reverseArray.length;
+const newRev = reverseArray.slice(1);
+const newRevToString = newRev.join('').toString();
+//console.log(Number(newRevToString));
+//End of Long cut 
 
-function reverse(string){
-    // Checking the Conditions First.
+// The shortWay
+const a = y.reverse().join('').toString();
 
-    if(!string || string.length < 2 || typeof string != 'string' ){
-        // If any of the condition satisfies return the string as it is.
-        return string;
-    } 
+//console.log(Number(a));
 
-    const backward =[];
-    const total_item = string.length -1; // bcoz it going to start from 0.
 
-    for(var i = total_item; i >=0 ; i--){
-        // Going backwards.
-        backward.push(string[i]);
-    }
+const myArr = [10,5,9,8,2];
+const sorted = myArr.sort();
 
-    // Join concatenating the elemnts in the Array.
-    return backward.join(''); 
+
+
+const getSize = ( param )=> {
+  const sorted = [...param].sort();
+   let newDoubleNum = [];
+   let single = [];
+for (let index = 0; index < sorted.length; index++) {
+   const element = sorted[index];
+
+   if (element > 9) {
+      newDoubleNum.push(element);  
+   }else{
+      single.push(element)
+   }
+}
+  // console.log(newDoubleNum, single);
+
+   const arrangedArray = [...single,...newDoubleNum];
+   //console.log(arrangedArray);
+   //console.log(arrangedArray[sorted.length - 2] + ' is the second Largest ' + arrangedArray[1] + ' is the second lowest');
+
 }
 
-// Reverse A string in short methods.
+getSize([1,3,7,4,8,9]);
 
-function revShort(string){
 
-    // First it splits the string into Array 
-    // Reverse() method bring last element to first 
-     // Join concatenating the elemnts in the Array. 
-     
-    return string.split('').reverse().join('');
+const dashed = ( param ) => {
+   const numArr = [...param];
+   const evenArr = [];
+   const oddArr = [];
+   for (let index = 0; index < numArr.length; index++) {
+      const element = array[index];
+      if(element % 2 == 1){
+         oddArr.push(element);
+      }else{
+         evenArr.push(element);
+      }
+
+      
+      
+   }
 }
 
-// Calling Functions.
-reverse('Algorithms');
 
-revShort("DataStruct");
+const ac = ['babatunde-hameed-olakunle']
+const trans = ac.toString()
+
+const xaa = trans.split('-')
+
+console.log(xaa);
