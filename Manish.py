cce3ddffcc3b62e3b9b736e6fbb0b15a65efc5e5
manish00-tom <61766579+manish00-tom@users.noreply.github.com>
2020-10-23 22:38:29 +0530

    Update and rename taran_kumar.md to Manish.py
    Day Calories calculator

---
+++ Manish.py
@@ -0,0 +1,7 @@
+a=input("Today's date?\n")
+b=int(input("Breakfast calories?\n"))
+c=int(input("Lunch calories?\n"))
+d=int(input("Dinner calories?\n"))
+e=int(input("Snack calories?\n"))
+t=b+c+d+e
+print("Calorie content for",a,":",t)
