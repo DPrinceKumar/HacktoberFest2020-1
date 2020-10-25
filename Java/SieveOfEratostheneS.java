5301e044a9f1d3cc6927b29aedc908b3d54333cc
adi2420 <68088764+adi2420@users.noreply.github.com>
2020-10-25 22:42:14 +0530

    Add files via upload

---
+++ Java/SieveOfEratostheneS.java
@@ -0,0 +1,46 @@
+import java.util.*; 
+
+ public class SieveOfEratosthenes 
+{ 
+	public static void sieveOfEratosthenes(int n) 
+	{ 
+		// Create a boolean array "prime[0..n]" and initialize 
+		// all entries it as true. A value in prime[i] will 
+		// finally be false if i is Not a prime, else true. 
+		boolean prime[] = new boolean[n+1]; 
+		for(int i=0;i<n;i++) 
+			prime[i] = true; 
+		
+		for(int p = 2; p*p <=n; p++) 
+		{ 
+			// If prime[p] is not changed, then it is a prime 
+			if(prime[p] == true) 
+			{ 
+				// Update all multiples of p 
+				for(int i = p*p; i <= n; i += p) 
+					prime[i] = false; 
+			} 
+		} 
+		
+		// Print all prime numbers 
+		for(int i = 2; i <= n; i++) 
+		{ 
+			if(prime[i] == true) 
+				System.out.print(i + " "); 
+		} 
+	} 
+	
+	// Driver Program to test above function 
+	public static void main(String args[]) 
+	{ 	
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number");
+		int n = sc.nextInt();
+		System.out.print("Following are the prime numbers "); 
+		System.out.println("smaller than or equal to " + n); 
+		SieveOfEratosthenes g = new SieveOfEratosthenes(); 
+		g.sieveOfEratosthenes(n); 
+	} 
+} 
+
+
