726fb52897903bba38acc3d9e085d31c6c579c39
Ana Flávia Frontino <61756289+anaflaviafcruz@users.noreply.github.com>
2020-10-28 16:40:09 -0300

    Update array.c

--- C/array.c
+++ C/array.c
@@ -12,7 +12,7 @@ int main()
                 scanf("%d",&marks[i][j]);
             }
 
-            printf("\n");
+            printf("Array: \n");
         } for(int i=0;i<2;i++)
         {
             for(int j=0;j<4;j++)
