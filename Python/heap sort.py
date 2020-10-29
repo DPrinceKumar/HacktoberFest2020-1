a9122f2ec7706ccdcd4e9bc6b79112d2d0c0e19a
BlackHat-TG <43718084+BlackHat-TG@users.noreply.github.com>
2020-10-29 10:47:51 +0530

    Add files via upload

---
+++ Python/heap sort.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+def heapify(arr, n, i): 
+	largest = i # Initialize largest as root 
+	l = 2 * i + 1	 # left = 2*i + 1 
+	r = 2 * i + 2	 # right = 2*i + 2 
+
+	if l < n and arr[i] < arr[l]: 
+		largest = l 
+
+	if r < n and arr[largest] < arr[r]: 
+		largest = r 
+
+	if largest != i: 
+		arr[i],arr[largest] = arr[largest],arr[i] # swap 
+
+		# Heapify the root. 
+		heapify(arr, n, largest) 
+
+# The main function to sort an array of given size 
+def heapSort(arr): 
+	n = len(arr) 
+    
+	for i in range(n // 2 - 1, -1, -1): 
+		heapify(arr, n, i) 
+
+	# One by one extract elements 
+	for i in range(n-1, 0, -1): 
+		arr[i], arr[0] = arr[0], arr[i] # swap 
+		heapify(arr, i, 0) 
+
+# Driver code to test above 
+arr = [ 12, 11, 13, 5, 6, 7] 
+heapSort(arr) 
+n = len(arr) 
+print ("Sorted array is") 
+for i in range(n): 
+	print ("%d" %arr[i]), 
+# This code is contributed by Mohit Kumra 
+
+
+# In[ ]:
+
+
+
+
