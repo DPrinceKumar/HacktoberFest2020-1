88a8715af19543ee7ee910d87b69c7595abc3937
Surya Dev Singh <46717624+suryadevsingh@users.noreply.github.com>
2020-10-30 09:53:45 +0530

    Create restApiInGolang.go
    rest api in golang. @hacktoberfest2020 #hacktoberfest2020

---
+++ restApiInGolang.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "Welcome to the HomePage!")
+    fmt.Println("Endpoint Hit: homePage")
+}
+
+func handleRequests() {
+    http.HandleFunc("/", homePage)
+    log.Fatal(http.ListenAndServe(":10000", nil))
+}
+
+func main() {
+    handleRequests()
+}
