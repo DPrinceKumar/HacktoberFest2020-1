f7dcc2fe56ab874bcf69b07823454c50d46e7087
Edewor Onyedika <edwordkaso100@gmail.com>
2020-10-31 22:06:21 +0100

    using javascript map method

---
+++ Javascript/map.js
@@ -0,0 +1,6 @@
+// Using Javascript Map array method
+// map() method is used for creating a new array from an existing one
+
+const numbers = [1, 2, 3, 4];
+const doubled = numbers.map(item => item * 2);
+console.log(doubled); // [2, 4, 6, 8]
