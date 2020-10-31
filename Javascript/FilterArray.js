7ac302568c2ab97c2312805def2a28eea0cbc2bd
kri99 <24420120+kri99@users.noreply.github.com>
2020-10-31 13:44:06 +0100

    Delete FilterArray.js

--- Javascript/FilterArray.js
+++
@@ -1,7 +0,0 @@
-// filters array by function and return only values which are not meet the condition
-const filter = (cond, arr) => arr.filter((...arg) => !cond(...arg));
-
-
-// example - filter array and return words which have capital first letter
-console.log(filter(w => w[0] === w[0].toLowerCase(), ['ask', 'Adam','car','Bell','town', 'Computer', 'Javascript']));
-// ["Adam", "Bell", "Computer", "Javascript"]
